34f62cadbab9f2e0da9b6d78a70d7c94ddc37ca0
Grzegorz Dziechciarz <grzes.dziechciarz@gmail.com>
2024-03-30 23:14:43 +0100

    added args parsing and option for non linux hosts

--- passBreaker.py
+++ passBreaker.py
@@ -1,9 +1,14 @@
 from pwn import *  # type: ignore  
+import argparse
 
 def check_env():
     if not term.can_init():
-        print('try using linux env, logging might not look great because term could not be enabled ')
+        print('try using linux env, logging might not look great because pwnlib term could not be enabled ')
         exit()
 
 if __name__ == '__main__':
-    check_env()
+    parser = argparse.ArgumentParser(prog='passBraker.py', description='simple script displaying how easy it is to break weak passwords' )
+    parser.add_argument('--no-env-check', help='check to allow running on env that does not support pwnlib term', action='store_true', dest='no_env_check')
+    args = parser.parse_args()
+    if not args.no_env_check:
+        check_env()
