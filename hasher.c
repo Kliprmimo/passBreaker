a1342ea12151512e4c4361b1e31b85ba06c2e59d
Kliprmimo <grzes.dziechciarz@gmail.com>
2024-04-05 16:04:48 +0200

    change to integrate with wrapper nicely

--- hasher.c
+++ hasher.c
@@ -3,25 +3,33 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include<unistd.h>
 
 #define BUFFER_SIZE 256
+// compile using:
+// gcc hasher.c -o hasher -I/usr/local/opt/openssl/include -L/usr/local/opt/openssl/lib -lssl -lcrypto
 
 int checker(FILE * file, unsigned
     const char * known_hash) {
     unsigned char pass_candidate[BUFFER_SIZE];
     unsigned char hash[SHA256_DIGEST_LENGTH];
+    unsigned int i = 0;
 
     while (fgets(pass_candidate, sizeof(pass_candidate), file) != NULL) {
         pass_candidate[strcspn(pass_candidate, "\r")] = 0;
         pass_candidate[strcspn(pass_candidate, "\n")] = 0;
         SHA256(pass_candidate, strlen(pass_candidate), hash);
 
-        // printf("Hash of '%s' is: ", pass_candidate);
-        // for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
-        //     printf("%02x", hash[i]);
-        // }
-
-        // printf("\n");
+        // print out hashe every 1k not to hinder performance and to make pogging in wrapper nice
+        if (i%1000 == 0){
+            // printf("Hash of '%s' is: ", pass_candidate);
+            for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+                printf("%02x", hash[i]);
+            }
+            usleep(100000);
+            printf("\n");
+        }
+        i++;
         int equal = 1;
         for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
             if (hash[i] != known_hash[i]) {
